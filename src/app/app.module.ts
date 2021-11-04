2ba556b1678580e85d2b7e0a4e5852d2d20fd87c
Sharon Cherop <sh.cherop@gmail.com>
2021-11-04 12:26:03 +0300

    adding wallet component and the qr code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { WalletComponent } from './wallet/wallet.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    WalletComponent
   ],
   imports: [
     BrowserModule,
